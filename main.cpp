7cf0ee86083ff3384692d8675266a27d93a7274f
Resham <resha@MacBook-Air-2.local>
2025-12-11 18:48:18 -0800

    Added node to the queue and displaying the list

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
 struct Node 
@@ -8,7 +9,8 @@ struct Node
     Node* next;
 };
 
-class CoffeeQueue {
+class CoffeeQueue 
+{
     private:
         Node* headNode;
         Node* tailNode;
@@ -18,14 +20,44 @@ class CoffeeQueue {
         {
             headNode = tailNode = nullptr;
         }
-    };
+
+        void addToQueue(string customerName, string drinkOrdered) 
+        {
+            Node* newNode = new Node{customerName, drinkOrdered, nullptr};
     
+            if (headNode == nullptr) 
+            { 
+                headNode = newNode;
+            } 
+            else 
+            {
+                tailNode->next = newNode;
+                tailNode = newNode;
+            }
+    
+            cout << customerName << " entered the line (ordered " << drinkOrdered << ")." << endl;
+        }
+
+        void displayList() 
+        {
+            cout << endl << "Current queue:" << endl;
+            if (headNode == nullptr) 
+            {
+                cout << "\tempty" << endl;
+            }
+            else
+            {
+                Node* curr = headNode;
+                while (curr != nullptr) 
+                {
+                    cout << "\t" << curr->customerName << " - " << curr->drinkOrdered << endl;
+                    curr = curr->next;
+                }
+            }
+        }
+    };
 
 int main()
 {
-
-
-
-    
     return 0;
 }
